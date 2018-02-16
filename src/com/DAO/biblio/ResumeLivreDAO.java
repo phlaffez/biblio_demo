79f8f92607498aee08d2400102771ca65a5b7b3e
phlaffez <phlaffez2@gmail.com>
2018-02-16 20:24:51 +0100

    Classe Genre() implementee, pas encore testee

--- src/com/DAO/biblio/ResumeLivreDAO.java
+++ src/com/DAO/biblio/ResumeLivreDAO.java
@@ -1,5 +1,5 @@
 package com.DAO.biblio;
 
-public class ResuleLivreDAO {
+public class ResumeLivreDAO {
 
 }
--- src/com/metier/biblio/Genre.java
+++ src/com/metier/biblio/Genre.java
@@ -1,5 +1,49 @@
 package com.metier.biblio;
 
 public class Genre {
-
-}
+	private int id;
+	private String nom_genre;
+	
+	public void Genre()
+	{
+		this.id = 0;
+		this.nom_genre="";
+	}
+	
+	public void Genre(int id,String ng)
+	{
+		this.id = id;
+		this.nom_genre=ng;
+	}
+	
+	// getters et setters
+	
+	public void setId(int id)
+	{
+		this.id=id;
+	}
+	
+	public int getId()
+	{
+		return this.id;
+	}
+	
+	public void setNomGenre(String ng)
+	{
+		this.nom_genre=ng;
+	}
+	
+	public String getNomGenre()
+	{
+		return this.nom_genre;
+	}
+	
+	
+	// tostring
+	
+	public String toSring()
+	{
+		return Integer.toString(this.id)+" "+this.nom_genre;
+	}
+	
+	}
