c7c98be901c5793261f70df5fbe954e465a635d0
phlaffez <phlaffez2@gmail.com>
2018-02-28 15:34:05 +0100

    Implementation sans test de la classe ReumeLivre()

--- src/com/metier/biblio/ResumeLivre.java
+++ src/com/metier/biblio/ResumeLivre.java
@@ -2,4 +2,55 @@ package com.metier.biblio;
 
 public class ResumeLivre {
 
+	private int id;
+	private String resume;    // resume et un champ text mysql. Un String est
+	                          // il suffisant ? A vérifier et modifier
+	                         // en conséquence si nécessaire:
+
+	
+	// constructeurs
+	
+	public void ResumeLivre()
+	{
+		this.id=0;
+		this.resume="";
+	}
+	
+	public void ResumeLivre(int id, String r)
+	{
+		this.id=id;
+		this.resume=r;
+	}
+	
+	// getters et setters:
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public int getId()
+	{
+		return this.id;
+	}
+	
+	public void setResume(String r)
+	{
+		this.resume=r;
+	}
+	
+	public String getResume()
+	{
+		return this.resume;
+	}
+	
+	public String toString()
+	{
+		String s;
+		s=Integer.toString(this.id)+":\n";
+		s=s+this.resume;
+		return s;
+		}
+	
+	
 }
