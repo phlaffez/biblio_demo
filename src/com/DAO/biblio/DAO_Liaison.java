b21db3ba43f6660c8bc114bf439b6dea2373bb7b
phlaffez <phlaffez2@gmail.com>
2018-03-06 11:54:26 +0100

    Modification de l'interface DAO_Liaison. Ajout de la méthode Object getListeByCleLiaison(Cles cle, int id) et de la méthode boolean deleteByCleLiaison(Cles cle, int id) qui restent à implémenter dans 3 classes

--- src/com/DAO/biblio/DAO_Liaison.java
+++ src/com/DAO/biblio/DAO_Liaison.java
@@ -12,6 +12,7 @@ import com.outils.biblio.Cles;
  */
 
 public interface DAO_Liaison <T>{
+	
 	public Object getByCleLiaison(Cles cle, int id);
 	/** Les tables de laison ont deux champs. cle représente le nom du champ
 	 * selon lequel se fera la recherche, id la valeur recherchée.
@@ -19,6 +20,29 @@ public interface DAO_Liaison <T>{
 	 * recherche dans le style SELECT cle1 FROM table WHERE cle2 = id
 	 * si la valeurcle1 est passée comme paramètre de clé
 	 * */
+	
+	public Object getListeByCleLiaison(Cles cle, int id);
+	/** renvoie une liste d'objet métier pour une clé donnée, par exemple tous
+	 * les livres de l'auteur id "id"
+	 * 
+	 * @param cle: la clé suivant laquelle on fait la recherche, par exemple id_auteur
+	 * @return une Liste d'objet correspondant à cette clé fixe, par exemple une
+	 * liste de livre pour la table livre_auteurs avec la clé id_auteur, ou la liste
+	 * des auteurs pour la même table avec la clé id_livre
+	 * id: la valeur de la clé
+	 * 
+	 * 
+	 */
+	
+	public boolean deleteByCleLiaison(Cles cle, int id);
+	/**
+	 * Permet d'effacer tous les enregistrement de la table de liaison pour 
+	 * une valeur de clé donnée
+	 * @param cle: la clé servant àla recherche. Par exemple id_livre pour supprimer
+	 * tous les auteurs d'un livre donné dans la table livre_auteurs
+	 * @param id  La valeur de la clé (identificateur de l'auteur dans l'exemple ci dessus
+	 * @return un boolean, true si ça s'est bien passé
+	 */
 	
 	public boolean isPresent(T obj);
 	
