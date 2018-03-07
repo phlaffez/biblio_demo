29a1a10eb8d4dad63a8202d1e1299992a0b7276a
phlaffez <phlaffez2@gmail.com>
2018-03-07 12:02:48 +0100

    Implementation des méthodes getListeByCleLiaison et deleteByCleLiaison de la classe AuteurLangue. A tester

--- src/com/DAO/biblio/AuteurLangueDAO.java
+++ src/com/DAO/biblio/AuteurLangueDAO.java
@@ -6,7 +6,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.metier.biblio.Auteur;
 import com.metier.biblio.AuteurLangue;
+import com.metier.biblio.Genre;
+import com.metier.biblio.Langue;
 import com.metier.biblio.Pays;
 import com.outils.biblio.Cles;
 
@@ -195,6 +198,81 @@ public class AuteurLangueDAO extends DAO<AuteurLangue> implements DAO_Liaison<Au
 		}
 		
 		return resultat;
+	}
+
+
+
+	@Override
+	public Object getListeByCleLiaison(Cles cle, int id) {
+		// récupérer la liste des entiers correspondants:
+		List<Integer> listeres = (List<Integer>)this.getByCleLiaison(cle, id);
+		if(listeres.size()==0)
+		{
+			return null;   // liste vide
+		}
+		else
+		{
+			if(cle==Cles.id_auteur)
+			{
+				// on retourne la liste des langues pratiquées par cet auteur
+				DAO langueDAO = new LangueDAO(connex);
+				List<Langue> langues = new ArrayList<Langue>();
+				for(int i=1;i<listeres.size();i++)
+				{
+					langues.add((Langue)langueDAO.findId(listeres.get(i)));
+				}
+				return langues;
+			}
+			else
+				
+			{
+				// on retourne la liste des auteurs écrivant dans cette langue
+				DAO auteurDAO = new AuteurDAO(connex);
+				List<Auteur> auteurs = new ArrayList<Auteur>();
+				for(int i = 1; i<listeres.size();i++)
+				{
+					auteurs.add(((Auteur)auteurDAO.findId(listeres.get(i))));
+				}
+				return auteurs;
+			}
+		}
+		}
+
+
+
+	@Override
+	public boolean deleteByCleLiaison(Cles cle, int id) {
+		String requete;
+		int res;
+		int mes=0;
+		// TODOUne requete à écrire en fonction de la clé de selection
+		if(cle == Cles.id_auteur)
+		{
+			// on efface tous les liaisons livre auteur de cet auteur
+			requete = "DELETE * FROM auteur_langue WHERE id_auteur = "+Integer.toString(id);
+		}
+		else
+		{
+			// le contraire, id on efface toutes les liaisons pour ce livre
+			requete = "DELETE FROM auteur_langue WHERE id_langue ="+Integer.toString(id);
+		}
+		boolean retour = false;
+		try
+		{
+			res = this.connex.createStatement(). executeUpdate(requete);
+			if(res==1)
+			{
+				retour = true;
+			}
+		}
+		catch (SQLException e)
+		{
+			System.out.println("Ereur SQLdans la suppression dans la tablede liaison auteurs langues");
+			System.out.println(cle.toString()+" = "+id);
+			mes=1;
+		}
+		
+		return retour;
 	}
 
 }
