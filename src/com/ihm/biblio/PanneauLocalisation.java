ba5edda241fed9dfb61aa84b91db36e253afcff4
phlaffez <phlaffez2@gmail.com>
2018-09-05 17:17:09 +0200

    Fin ecriture panneauLocalisation. Reste à vérifier qu'ill fonctionne comme voulu

--- src/com/ihm/biblio/PanneauLocalisation.java
+++ src/com/ihm/biblio/PanneauLocalisation.java
@@ -1,12 +1,16 @@
 package com.ihm.biblio;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JScrollPane;
 
 import com.DAO.biblio.BddTables;
 import com.DAO.biblio.DAOTableFactory;
 import com.DAO.biblio.DaoFactoryMySQL;
+import com.DAO.biblio.GenreDAO;
+import com.DAO.biblio.LocalisationDAO;
 import com.dbacces.biblio.Mysql_Connect;
 import com.ihm.biblio.PanneauGenre.BoutonAjoutListener;
 import com.metier.biblio.Genre;
@@ -57,5 +61,29 @@ this.setVisible(false);
 this.setVisible(true);
 		
 	}
+	
+	class BoutonAjoutListener implements ActionListener
+	{  
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String nom = champSaisie.getText().toUpperCase();
+		if(!nom.isEmpty())
+		{
+		LocalisationDAO dao = factory.getLocalisationDAO();
+		lieu = (Localisation)dao.getByNom(nom);
+		if(lieu==null)
+		{
+			lieu = new Localisation(0,nom);
+			boolean ok = dao.create(lieu);
+			panTable.remove(jsp);
+			initTable();
 
-}
+		}
+		else
+		{
+			System.out.println("Localisation existante dans la base de données. Faire un popup ici");
+		}
+		}	
+	}
+	}
+	}
